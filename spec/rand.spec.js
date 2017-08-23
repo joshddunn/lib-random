38660bb56a1d9964c9e549f01cb2f453d8fc1b28
Josh Dunn <jdunn45@alumni.uwo.ca>
2017-08-23 01:03:21 -0400

    Clean up tests to use Math.max and Math.min

--- spec/rand.spec.js
+++ spec/rand.spec.js
@@ -14,54 +14,44 @@ describe('RandJS:', () => {
         expect(r.seed).toEqual(1);
     });
 
-
     it('check default float values are between 0 and 1', () => {
         const r = new RandJS();
         var num = r.manyRand(1000);
 
-        num.forEach((e) => {
-            expect(e < 1).toBe(true);
-            expect(e >= 0).toBe(true);
-        });
+        expect(Math.max(...num) < 1).toBe(true);
+        expect(Math.min(...num) >= 0).toBe(true);
     });
 
     it('check float values are between 10 and 100', () => {
         const r = new RandJS();
         var num = r.manyRand(1000, 10, 100);
 
-        num.forEach((e) => {
-            expect(e < 100).toBe(true);
-            expect(e >= 10).toBe(true);
-        });
+        expect(Math.max(...num) < 100).toBe(true);
+        expect(Math.min(...num) >= 10).toBe(true);
     });
 
-
-    it('check default int values are between 0 and RAND_MAX', () => {
+    it('check default int values are between 0 and randMax', () => {
         const r = new RandJS();
         var num = r.manyRandInt(1000);
 
-        num.forEach((e) => {
-            expect(e < r.m).toBe(true);
-            expect(e >= 0).toBe(true);
-        });
+        expect(Math.max(...num) <= r.randMax).toBe(true);
+        expect(Math.min(...num) >= 10).toBe(true);
     });
 
     it('check int values are between 10 and 100, inclusive', () => {
         const r = new RandJS();
         var num = r.manyRandInt(1000, 10, 100);
 
-        num.forEach((e) => {
-            expect(e <= 100).toBe(true);
-            expect(e >= 10).toBe(true);
-        });
+        expect(Math.max(...num) <= 100).toBe(true);
+        expect(Math.min(...num) >= 10).toBe(true);
     });
 
     it('check int values are inclusive', () => {
         const r = new RandJS();
         var num = r.manyRandInt(1000, 10, 11);
 
-        expect(num.indexOf(10) === -1).toBe(false);
-        expect(num.indexOf(11) === -1).toBe(false);
+        expect(num.indexOf(10)).not.toBe(false);
+        expect(num.indexOf(11)).not.toBe(false);
     });
 
 });
--- src/rand.js
+++ src/rand.js
@@ -3,28 +3,12 @@ export default class RandJS {
     constructor(seed = Date.now()) {
         this.seed = seed;
         this.current = this.seed;
-        this.m = 2147483647;
-        this.a = 16807;
+        this.modulus = 2147483647;
+        this.multiplier = 16807;
     }
 
-    next() {
-        return this.current = (this.current * this.a) % this.m;
-    }
-
-    rand(a = 0, b = 1) {
-        return a + (b - a) * this.next() / this.m;
-    }
-
-    randInt(a = 0, b = this.m - 1) {
-        return a + this.next() % (b + 1 - a);
-    }
-
-    manyRand(n, a = 0, b = 1) {
-        return this._many(n, () => this.rand(a, b));
-    }
-
-    manyRandInt(n, a = 0, b = this.m - 1) {
-        return this._many(n, () => this.randInt(a, b));
+    _next() {
+        return this.current = (this.current * this.multiplier) % this.modulus;
     }
 
     _many(n, func, gen = []) {
@@ -32,7 +16,27 @@ export default class RandJS {
         return gen.length === n ? gen : this._many(n, func, gen);
     }
 
+    rand(a = 0, b = 1) {
+        return a + (b - a) * this._next() / this.modulus;
+    }
+
+    randInt(a = 0, b = this.modulus - 1) {
+        return a + this._next() % (b + 1 - a);
+    }
+
+    manyRand(n, a = 0, b = 1) {
+        return this._many(n, () => this.rand(a, b));
+    }
+
+    manyRandInt(n, a = 0, b = this.modulus - 1) {
+        return this._many(n, () => this.randInt(a, b));
+    }
+
     static seed() {
         return this.seed;
+    }
+
+    get randMax() {
+        return this.modulus;
     }
 }
